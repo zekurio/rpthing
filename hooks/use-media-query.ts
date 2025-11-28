3000b2d46d3cb7634a899c3413fd2f6f66289a96
Michael Schwieger <git@zekurio.xyz>
2025-11-28 18:57:54 +0100

    fix(format): formatting fixed
    Signed-off-by: Michael Schwieger <git@zekurio.xyz>

--- hooks/use-media-query.ts
+++ hooks/use-media-query.ts
@@ -1,22 +1,22 @@
 import * as React from "react";
 
 export function useMediaQuery(query: string): boolean {
-    const [matches, setMatches] = React.useState<boolean>(false);
+	const [matches, setMatches] = React.useState<boolean>(false);
 
-    React.useEffect(() => {
-        if (typeof window === "undefined") return;
-        const mql = window.matchMedia(query);
-        const onChange = (e: MediaQueryListEvent) => {
-            setMatches(e.matches);
-        };
+	React.useEffect(() => {
+		if (typeof window === "undefined") return;
+		const mql = window.matchMedia(query);
+		const onChange = (e: MediaQueryListEvent) => {
+			setMatches(e.matches);
+		};
 
-        // Set initial value
-        setMatches(mql.matches);
+		// Set initial value
+		setMatches(mql.matches);
 
-        // Listen for changes
-        mql.addEventListener("change", onChange);
-        return () => mql.removeEventListener("change", onChange);
-    }, [query]);
+		// Listen for changes
+		mql.addEventListener("change", onChange);
+		return () => mql.removeEventListener("change", onChange);
+	}, [query]);
 
-    return matches;
+	return matches;
 }
